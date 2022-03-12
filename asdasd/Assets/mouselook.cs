4f85ca36dc6ae79dfd87a7f075db604f4d34ac30
Sebastian <zyoxpus@hotmail.com>
2022-03-12 19:49:28 +0100

    yes

---
+++ asdasd/Assets/mouselook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+
+    public float MouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
